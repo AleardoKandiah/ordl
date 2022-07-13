f83bd51a6c6758cfc1509edab8d8a2b5b4e077cf
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-13 14:22:58 +0100

    solution word

--- components/SolutionWordContext.tsx
+++ components/SolutionWordContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 
+// store solution word
 export const SolutionWordContext = createContext({
   solutionWord: "",
   setSolutionWord: (_: string) => {},
