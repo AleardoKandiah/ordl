14dd94ee860fb5662bed288c36567e237021a39e
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-14 16:10:57 +0100

    word evaluation

--- evaluate.ts
+++ evaluate.ts
@@ -7,7 +7,8 @@ export const evaluate = (
   // accuracies[i] corresponds to letter i
   const accuracies: Accuracy[] = new Array(guessWord.length).fill("wrong");
 
-
+  // Need frequency map to handle letter duplicates in guess/solution word.
+  // e.g., with solutionWord "ae" and guessWord "ee", the boxes should be "grey, green".
   const freqs: { [key: string]: number } = {};
   for (const letter of solutionWord) {
     if (!(letter in freqs)) {
